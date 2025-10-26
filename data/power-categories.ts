0b5a7dc67af338f24f0fa0c6f998d962852e318f
oClaus <claudiofradiquej@gmail>
2025-10-26 16:37:38 -0300

    exclui poder geral

--- data/power-categories.ts
+++ data/power-categories.ts
@@ -8,11 +8,4 @@ export const powerCategories: PowerCategory[] = [
     description: "Poderes exclusivos da classe Arcanista, focados em magia arcana e manipulação de energia mágica.",
     origin: "tormenta20 - jogo do ano"
   },
-  {
-    id: "2",
-    name: "Poderes Gerais",
-    slug: "poderes-gerais",
-    description: "Poderes disponíveis para todas as classes, oferecendo melhorias gerais de combate e defesa.",
-    origin: "tormenta20 - jogo do ano"
-  }
 ];
